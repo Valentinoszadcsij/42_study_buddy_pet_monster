2512bba365e997dee0925b07bb5e87c7bd06357c
SuPuHe <mizin.a.s.2006@gmail.com>
2025-10-21 14:21:24 +0200

    fix

--- src/App/Utility/config.php
+++ src/App/Utility/config.php
@@ -1,12 +1,10 @@
 <?php
 
-require_once __DIR__ . '/env.php';
-
-return [
+return[
     'client_id' => getenv('CLIENT_ID') ?: $_ENV['CLIENT_ID'] ?? '',
     'client_secret' => getenv('CLIENT_SECRET') ?: $_ENV['CLIENT_SECRET'] ?? '',
-    'redirect_uri' => getenv('REDIRECT_URI') ?: $_ENV['REDIRECT_URI'] ?? 'https://localhost:8443/Auth/callback',
-    'authorize_url' => getenv('AUTHORIZE_URL') ?: $_ENV['AUTHORIZE_URL'] ?? 'https://api.intra.42.fr/oauth/authorize',
-    'token_url' => getenv('TOKEN_URL') ?: $_ENV['TOKEN_URL'] ?? 'https://api.intra.42.fr/oauth/token',
-];
+    'redirect_uri' => 'http://168.119.183.211:8443/Auth/callback',
+    'authorize_url' => 'https://api.intra.42.fr/oauth/authorize',
+    'token_url' => 'https://api.intra.42.fr/oauth/token',
+]
 ?>
