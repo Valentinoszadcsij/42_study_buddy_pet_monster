c365d2fbfb791517460c27d7b10008d1e32dc7cc
SuPuHe <mizin.a.s.2006@gmail.com>
2025-10-21 14:39:23 +0200

    fix

--- src/App/Utility/config.php
+++ src/App/Utility/config.php
@@ -3,7 +3,7 @@
 return[
     'client_id' => getenv('CLIENT_ID') ?: $_ENV['CLIENT_ID'] ?? '',
     'client_secret' => getenv('CLIENT_SECRET') ?: $_ENV['CLIENT_SECRET'] ?? '',
-    'redirect_uri' => 'https://168.119.183.211:8443/Auth/callback',
+    'redirect_uri' => 'http://168.119.183.211:8080/Auth/callback',
     'authorize_url' => 'https://api.intra.42.fr/oauth/authorize',
     'token_url' => 'https://api.intra.42.fr/oauth/token',
 ]
