4ea9d66c437996b77938dc804793945ab877970f
Valentin Oszadcsij <valentinosadchiy@gmail.com>
2025-10-21 10:03:27 +0200

    first endpoint

--- src/App/Controllers/ApiController.php
+++ src/App/Controllers/ApiController.php
@@ -10,9 +10,19 @@ class ApiController
 
     public function getMochiStats()
     {
-        return json_encode([
-            'hp' => $_SESSION['user_info']['first_name'],
+        if (!isset($_SESSION['hp'])) $_SESSION['hp'] = 100;
+        if (!isset($_SESSION['days'])) $_SESSION['days'] = 0;
+        if (!isset($_SESSION['coins'])) $_SESSION['coins'] = 10;
+        if (!isset($_SESSION['int_food'])) $_SESSION['int_food'] = 0;
+        if (!isset($_SESSION['char_food'])) $_SESSION['char_food'] = 0;
 
+        header('Content-Type: application/json');
+        echo json_encode([
+            'hp' => $_SESSION['hp'],
+            'days' => $_SESSION['days'],
+            'coins' => $_SESSION['coins'],
+            'int_food' => $_SESSION['int_food'],
+            'char_food' => $_SESSION['char_food']
         ]);
     }
 }
