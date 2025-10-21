b5a52c4ccd715450355f15a37c2105529e7e6567
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2025-10-21 14:53:01 +0200

    auth

--- src/App/Utility/config.php
+++ src/App/Utility/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return[
-    'client_id' => 'u-s4t2ud-fdd457c027e1aa1adf5fa99a740eaeb73bcaa23b0eb24d41c4fc02019aea917a',
-    'client_secret' => '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',
+    'client_id' => '',
+    'client_secret' => '',
     'redirect_uri' => 'https://localhost:8443/Auth/callback',
     'authorize_url' => 'https://api.intra.42.fr/oauth/authorize',
     'token_url' => 'https://api.intra.42.fr/oauth/token',
